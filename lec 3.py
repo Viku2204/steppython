64afb308f9b62bc677153498d54f72d4d7c631e3
viku2204] <vikuu2204@gmail.com>
2025-07-03 09:18:29 +0530

    list

---
+++ lec 3.py
@@ -0,0 +1,6 @@
+list=[3,5,2,6,4,8,1]
+amax=[0]
+bmin=[0]
+for i in list:
+    if i>0  and i<i[1]:
+        print("min value till index 1 is",bmin )
---
+++ lecc 4.py
@@ -0,0 +1,3 @@
+tuple=(1,3,3,7)
+print(tuple[0:2])
+print(tuple[2:4])
